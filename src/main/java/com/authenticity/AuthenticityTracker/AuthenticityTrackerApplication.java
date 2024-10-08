7d06c013e79ac3b46447325c4afba6c6ccd7868b
rookieanvesh <srivastavaanvesh13@gmail.com>
2024-10-08 17:44:36 +0530

    first

---
+++ src/main/java/com/authenticity/AuthenticityTracker/AuthenticityTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.authenticity.AuthenticityTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticityTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticityTrackerApplication.class, args);
+	}
+
+}
